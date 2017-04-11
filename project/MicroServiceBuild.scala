6be8169f8b6e0302dd7cde0e517c9a64e0f06c7b
Jenkins HMRC <jenkins-hmrc@digital.cabinet-office.gov.uk>
2017-04-11 10:23:48 +0000

    Creating new service soft-drinks-industry-levy

---
+++ project/MicroServiceBuild.scala
@@ -0,0 +1,32 @@
+import sbt._
+import play.sbt.PlayImport._
+import play.core.PlayVersion
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object MicroServiceBuild extends Build with MicroService {
+
+  val appName = "soft-drinks-industry-levy"
+
+  override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
+
+  val compile = Seq(
+    ws,
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "5.15.0",
+    "uk.gov.hmrc" %% "play-authorisation" % "4.3.0",
+    "uk.gov.hmrc" %% "play-health" % "2.1.0",
+    "uk.gov.hmrc" %% "play-url-binders" % "2.1.0",
+    "uk.gov.hmrc" %% "play-config" % "4.3.0",
+    "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
+    "uk.gov.hmrc" %% "domain" % "4.1.0"
+  )
+
+  def test(scope: String = "test,it") = Seq(
+    "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
+    "org.scalatest" %% "scalatest" % "2.2.6" % scope,
+    "org.pegdown" % "pegdown" % "1.6.0" % scope,
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+  )
+
+}
