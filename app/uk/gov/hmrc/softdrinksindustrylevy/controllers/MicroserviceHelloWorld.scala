2d0db385a1b7a379de613d4a5ff2a5735e51b39b
Luke Smith <luke.smith@digital.hmrc.gov.uk>
2017-04-12 16:26:14 +0100

    Added template service, connector and controller

--- app/uk/gov/hmrc/softdrinksindustrylevy/controllers/MicroserviceHelloWorld.scala
+++ app/uk/gov/hmrc/softdrinksindustrylevy/controllers/MicroserviceHelloWorld.scala
@@ -1,15 +1,38 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.softdrinksindustrylevy.controllers
 
-import uk.gov.hmrc.play.microservice.controller.BaseController
-import uk.gov.hmrc.play.http.logging.MdcLoggingExecutionContext._
+import javax.inject.{Inject, Singleton}
+
+import play.api.libs.json._
 import play.api.mvc._
-import scala.concurrent.Future
+import uk.gov.hmrc.play.microservice.controller.BaseController
+import uk.gov.hmrc.softdrinksindustrylevy.connectors.DesConnector
+import uk.gov.hmrc.softdrinksindustrylevy.modelsFormat._
+import uk.gov.hmrc.softdrinksindustrylevy.services.DesSubmissionService
+import scala.concurrent.ExecutionContext.Implicits.global
 
-object MicroserviceHelloWorld extends MicroserviceHelloWorld
-
-trait MicroserviceHelloWorld extends BaseController {
+@Singleton
+class MicroserviceHelloWorld @Inject() (val desSubmissionService: DesSubmissionService,
+																			  val desConnector: DesConnector) extends BaseController {
 
 	def hello() = Action.async { implicit request =>
-		Future.successful(Ok("Hello world"))
+		desConnector.submitDesRequest(desSubmissionService.buildDesSubmission()).map {
+			response => Ok(Json.toJson(response))
+		}
 	}
 }
