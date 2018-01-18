1bc88beb182199c403e3078294ba1e0bc1c6790a
Richard Dennehy <richard.dennehy@outlook.com>
2018-01-18 11:21:13 +0000

    Sdil 1022 pending queue check (#46)
    * SDIL-1022 log and raise deskpro ticket when a submission is pending for more than 3 hours
    
    * SDIL-1022 refactor to send overdue submissions via deskpro
    
    * SDIL-1022 add additional config to be able to turn the job off

---
+++ app/uk/gov/hmrc/softdrinksindustrylevy/connectors/ContactFrontendConnector.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.softdrinksindustrylevy.connectors
+
+import java.time.LocalDateTime
+
+import play.api.libs.json.Json
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.play.config.ServicesConfig
+import uk.gov.hmrc.softdrinksindustrylevy.config.WSHttp
+import uk.gov.hmrc.softdrinksindustrylevy.models.Subscription
+import uk.gov.hmrc.softdrinksindustrylevy.models.json.internal.subscriptionFormat
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class ContactFrontendConnector extends ServicesConfig {
+  val http: WSHttp = WSHttp
+
+  lazy val contactFrontendUrl: String = baseUrl("contact-frontend")
+
+  def raiseTicket(subscription: Subscription, safeId: String, timestamp: LocalDateTime)
+                 (implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Unit] = {
+
+    val headers = hc.withExtraHeaders("Csrf-Token" -> "nocheck")
+
+    //nothing useful we can do with this
+    val resubmitUrl = "/"
+
+    val payload = Map(
+      "contact-name" -> Seq("SDIL service"),
+      "contact-email" -> Seq("sdil.service.email@somewhere.com"),
+      "contact-comments" ->
+        Seq(
+          s"""
+             |Subscription stuck in pending queue:
+             |
+             |${Json.prettyPrint(Json.toJson(subscription))}
+             |
+             |Safe Id: $safeId
+             |
+             |Submitted at: $timestamp
+           """.stripMargin
+        ),
+      "isJavascript" -> Seq("false"),
+      "referer" -> Seq("soft-drinks-industry-levy"),
+      "csrfToken" -> Seq("nocheck"),
+      "service" -> Seq("soft-drinks-industry-levy")
+    )
+
+    val submitUrl = s"$contactFrontendUrl/contact/contact-hmrc/form?resubmitUrl=$resubmitUrl"
+
+    http.POSTForm[HttpResponse](submitUrl, payload)(implicitly, headers, ec) map { _ => () }
+  }
+}
