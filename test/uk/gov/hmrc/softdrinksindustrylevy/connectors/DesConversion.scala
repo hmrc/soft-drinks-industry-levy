a821193781efe820dede68f5b3a3fe02a4449b33
Richard Dennehy <richard.dennehy@digital.hmrc.gov.uk>
2018-04-09 14:51:50 +0100

    SDIL-1315 add small producer validation endpoint

--- test/uk/gov/hmrc/softdrinksindustrylevy/connectors/DesConversion.scala
+++ test/uk/gov/hmrc/softdrinksindustrylevy/connectors/DesConversion.scala
@@ -247,15 +247,25 @@ class DesConversionSpec extends FunSuite with PropertyChecks with Matchers {
     assert((producerDetails \ "voluntarilyRegistered").asOpt[Boolean].isEmpty)
   }
 
-  private lazy val baseSubscription = Subscription("1111111111",
-    "I AM THE ORGANISATION TO RULE",
-    Some("3"), UkAddress(List("6A Gunson Street, South East London"), "SE79 6NF"),
-    InternalActivity(Map.empty, isLarge = false),
-    LocalDate.parse("2018-04-06"),
-    List(Site(UkAddress(List("99 Burntscarthgreen", "North West London"), "NW33 9CV"), None)),
-    List(Site(UkAddress(List("128 Willowbank Close", "Bristol"), "BS78 5CB"), None),
-      Site(UkAddress(List("17 Trebarthen Terrace", "Northampton"), "NN08 2CC"), None)),
-    Contact(Some("Evelyn Hindmarsh"),
-      Some("pimirzalvqlsljtiwgIiqzljnKpofqguhwKiwkcbzfoykggiwskbarsbikwwfsgI"), "00779 705682", "nkzkjldisu@zmzlddpexr.co.uk"))
+  private lazy val baseSubscription = Subscription(
+    utr = "1111111111",
+    orgName = "I AM THE ORGANISATION TO RULE",
+    orgType = Some("3"),
+    address = UkAddress(List("6A Gunson Street, South East London"), "SE79 6NF"),
+    activity = InternalActivity(Map.empty, isLarge = false),
+    liabilityDate = LocalDate.parse("2018-04-06"),
+    productionSites = List(Site(UkAddress(List("99 Burntscarthgreen", "North West London"), "NW33 9CV"), None)),
+    warehouseSites = List(
+      Site(UkAddress(List("128 Willowbank Close", "Bristol"), "BS78 5CB"), None),
+      Site(UkAddress(List("17 Trebarthen Terrace", "Northampton"), "NN08 2CC"), None)
+    ),
+    contact = Contact(
+      Some("Evelyn Hindmarsh"),
+      Some("pimirzalvqlsljtiwgIiqzljnKpofqguhwKiwkcbzfoykggiwskbarsbikwwfsgI"),
+      "00779 705682",
+      "nkzkjldisu@zmzlddpexr.co.uk"
+    ),
+    endDate = None
+  )
 
 }
--- test/uk/gov/hmrc/softdrinksindustrylevy/connectors/package.scala
+++ test/uk/gov/hmrc/softdrinksindustrylevy/connectors/package.scala
@@ -115,7 +115,7 @@ package object gen {
     warehouseSites <- Gen.listOf(genUkAddress map addressToSite)
     contact <- genContact
   } yield Subscription(utr, orgName, Some(orgType), address, activity, liabilityDate,
-    productionSites, warehouseSites, contact)
+    productionSites, warehouseSites, contact, None)
 
   val genSite: Gen[Site] = for {
     ref <- Gen.oneOf("a", "b")
@@ -133,7 +133,7 @@ package object gen {
       warehouseSites <- Gen.listOf(genUkAddress map addressToSite)
       contact <- genContact
     } yield Subscription(utr, orgName, None, address, activity, liabilityDate,
-      productionSites, warehouseSites, contact)
+      productionSites, warehouseSites, contact, Some(liabilityDate.plusYears(1)))
   }
 
   val genSdil: Gen[String] = {
