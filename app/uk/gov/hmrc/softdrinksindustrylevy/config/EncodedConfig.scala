42c921255a0cf937fbcd42b6d641160baa66729d
adamdye-hmrc <31273792+adamdye-hmrc@users.noreply.github.com>
2018-07-10 10:50:07 +0100

    Sdil 1762 data correction (#108)
    * Initial version
    
    * enable overwriting of records
    
    * Add in encoding for config lines

---
+++ app/uk/gov/hmrc/softdrinksindustrylevy/config/EncodedConfig.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.softdrinksindustrylevy.config
+
+import com.typesafe.config._
+import com.typesafe.config.impl.Parseable
+import scala.collection.JavaConverters._
+import scala.collection.JavaConverters._
+import java.util.Base64
+
+object EncodedConfig {
+
+  lazy val decoder = Base64.getDecoder
+
+  def b64encode(in: String): String =
+    Base64.getEncoder.encodeToString(in.getBytes("UTF-8"))
+
+  def apply(in: Config): Config = {
+    in.entrySet.asScala.foldLeft(in){case (config, entry) =>
+      val key = entry.getKey
+      if (key.endsWith("-b64encoded")) {
+        val newkey = key.replace("-b64encoded","")
+        val value = new String(decoder.decode(in.getString(key)), "UTF-8")
+        val newVal = Parseable.newString(value, ConfigParseOptions.defaults).parse()
+        config.withValue(key.replace("-b64encoded",""), newVal)
+      } else
+        config
+    }
+  }
+}
--- app/uk/gov/hmrc/softdrinksindustrylevy/config/PlayWiring.scala
+++ app/uk/gov/hmrc/softdrinksindustrylevy/config/PlayWiring.scala
@@ -16,7 +16,7 @@
 
 package uk.gov.hmrc.softdrinksindustrylevy.config
 
-import java.time.Clock
+import java.time.{Clock, LocalDate}
 
 import akka.stream.Materializer
 import play.api.{Configuration, Environment}
@@ -26,6 +26,11 @@ import play.api.inject.ApplicationLifecycle
 
 import scala.concurrent.ExecutionContext
 import uk.gov.hmrc.softdrinksindustrylevy.services.DataCorrector
+import pureconfig._
+import pureconfig.configurable._
+import java.time.format._
+
+import sdil.models.SdilReturn
 
 trait PlayWiring {
   def applicationLifecycle: ApplicationLifecycle
@@ -38,20 +43,14 @@ trait PlayWiring {
 
   lazy val mode: Mode = environment.mode
 
-  val config: SdilConfig = {
-    import pureconfig._
-    import pureconfig.configurable._
-    import java.time.format._
-    implicit val localDateInstance = localDateConfigConvert(DateTimeFormatter.ISO_DATE)
-
-    implicit val litreageConfigReader = ConfigReader[List[Long]].map {case (l::h::_) => (l,h)}
-    implicit val hint = ProductHint[sdil.models.SdilReturn](allowUnknownKeys = false)
-
-    pureconfig.loadConfig[SdilConfig] match {
+  lazy val config: SdilConfig = {
+    implicit val localDateInstance: ConfigConvert[LocalDate] = localDateConfigConvert(DateTimeFormatter.ISO_DATE)
+    implicit val litreageConfigReader: ConfigReader[(Long, Long)] = ConfigReader[List[Long]].map {case (l::h::_) => (l,h)}
+    implicit val hint: ProductHint[SdilReturn] = ProductHint[sdil.models.SdilReturn](allowUnknownKeys = false)
+    
+    pureconfig.loadConfig[SdilConfig](EncodedConfig(configuration.underlying)) match {
       case Left(error) => throw new IllegalStateException(error.toString)
       case Right(conf) => conf
     }
   }
-
-
 }
