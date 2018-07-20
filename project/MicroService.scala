a4ba3e672dcc41b5d9e1cafba74a264ba0af3870
Luke Tebbs <luke.tebbs@digital.hmrc.gov.uk>
2018-07-20 13:10:56 +0100

    All non-breaking upgrades applied

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -19,11 +19,11 @@ object MicroService extends Build {
     "uk.gov.hmrc" %% "simple-reactivemongo" % "6.1.0",
     "uk.gov.hmrc" %% "auth-client" % "2.6.0",
     "uk.gov.hmrc" %% "mongo-lock" % "5.1.0",
-    "com.softwaremill.macwire" %% "macros" % "2.3.0" % "provided",
-    "com.softwaremill.macwire" %% "macrosakka" % "2.3.0" % "provided",
-    "com.softwaremill.macwire" %% "util" % "2.3.0",
-    "com.softwaremill.macwire" %% "proxy" % "2.3.0",
-    "org.typelevel" %% "cats-core" % "0.9.0",
+    "com.softwaremill.macwire" %% "macros" % "2.3.1" % "provided",
+    "com.softwaremill.macwire" %% "macrosakka" % "2.3.1" % "provided",
+    "com.softwaremill.macwire" %% "util" % "2.3.1",
+    "com.softwaremill.macwire" %% "proxy" % "2.3.1",
+    "org.typelevel" %% "cats-core" % "1.1.0",
     "com.github.fge" % "json-schema-validator" % "2.2.6",
     "com.github.pureconfig" %% "pureconfig" % "0.9.1"
   )
@@ -31,16 +31,16 @@ object MicroService extends Build {
   lazy val test = Seq(
     "org.scalatest" %% "scalatest" % "2.2.6" % "test",
     "com.typesafe.play" %% "play-test" % PlayVersion.current % "test",
-    "org.mockito" % "mockito-core" % "2.13.0" % "test",
+    "org.mockito" % "mockito-core" % "2.19.1" % "test",
     "org.pegdown" % "pegdown" % "1.6.0" % "test",
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % "test",
-    "org.scalacheck" %% "scalacheck" % "1.13.4" % "test",
-    "uk.gov.hmrc" %% "stub-data-generator" % "0.4.0" % "test",
-    "com.fasterxml.jackson.core" % "jackson-core" % "2.8.1" % "test",
-    "com.fasterxml.jackson.core" % "jackson-databind" % "2.8.1" % "test",
-    "com.github.tomakehurst" % "wiremock" % "2.14.0" % "test",
+    "org.scalacheck" %% "scalacheck" % "1.14.0" % "test",
+    "uk.gov.hmrc" %% "stub-data-generator" % "0.5.3" % "test",
+    "com.fasterxml.jackson.core" % "jackson-core" % "2.8.11" % "test",
+    "com.fasterxml.jackson.core" % "jackson-databind" % "2.8.11.2" % "test",
+    "com.github.tomakehurst" % "wiremock" % "2.18.0" % "test",
     "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % "test",
-    "org.jsoup" % "jsoup" % "1.11.2" % "test"
+    "org.jsoup" % "jsoup" % "1.11.3" % "test"
   )
 
   lazy val microservice = Project("soft-drinks-industry-levy", file("."))
