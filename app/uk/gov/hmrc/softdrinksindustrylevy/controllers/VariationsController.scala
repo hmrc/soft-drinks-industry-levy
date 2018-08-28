ab271843c50cbf2b3876cce1df6c7feab10bc0c2
Adam Dye <adam.dye@digital.hmrc.gov.uk>
2018-08-28 11:25:47 +0100

    Added the correct returns varition service to controller.

--- app/uk/gov/hmrc/softdrinksindustrylevy/controllers/VariationsController.scala
+++ app/uk/gov/hmrc/softdrinksindustrylevy/controllers/VariationsController.scala
@@ -24,52 +24,46 @@ import play.api.mvc.Action
 import uk.gov.hmrc.play.bootstrap.controller.BaseController
 import uk.gov.hmrc.softdrinksindustrylevy.connectors.GformConnector
 import uk.gov.hmrc.softdrinksindustrylevy.models.{ReturnsVariationRequest, VariationsRequest}
-import uk.gov.hmrc.softdrinksindustrylevy.services.VariationSubmissionService
+import uk.gov.hmrc.softdrinksindustrylevy.services.{ReturnsVariationSubmissionService, VariationSubmissionService}
 import uk.gov.hmrc.softdrinksindustrylevy.models.json.des.create.addressFormat
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import sys.process._
 
 
-
-class VariationsController(val messagesApi: MessagesApi,
-                           gforms: GformConnector,
-                           submissions: VariationSubmissionService) extends BaseController with I18nSupport {
+class VariationsController(
+  val messagesApi: MessagesApi,
+  gforms: GformConnector,
+  variationSubmissions: VariationSubmissionService,
+  returnSubmission: ReturnsVariationSubmissionService
+) extends BaseController with I18nSupport {
 
   def generateVariations(sdilNumber: String): Action[JsValue] = Action.async(parse.json) { implicit request =>
     withJsonBody[VariationsRequest] { data =>
       val page = views.html.variations_pdf(data, sdilNumber).toString
-      val temp = java.io.File.createTempFile("variations", ".html")
-
-      println(s"Writing to $temp")
-
-      new PrintWriter(temp) { write(page); close }
-
-      s"epiphany $temp".!
-      concurrent.Future.successful(NoContent)
-//      for {
-//        _ <- gforms.submitToDms(page, sdilNumber)
-//        _ <- submissions.save(data, sdilNumber)
-//      } yield NoContent
+      for {
+        _ <- gforms.submitToDms(page, sdilNumber)
+        _ <- variationSubmissions.save(data, sdilNumber)
+      } yield NoContent
     }
   }
 
   def returnsVariation(sdilNumber: String): Action[JsValue] = Action.async(parse.json) { implicit request =>
     withJsonBody[ReturnsVariationRequest] { data =>
       val page = views.html.returns_variation_pdf(data, sdilNumber).toString
-      val temp = java.io.File.createTempFile("variations", ".html")
-
-      println(s"Writing to $temp")
-
-      new PrintWriter(temp) { write(page); close }
-
-      s"epiphany $temp".!
-
-      concurrent.Future.successful(NoContent)
-//      for {
-////        _ <- gforms.submitToDms(page, sdilNumber)
-//        _ <- submissions.save(data, sdilNumber)
-//      } yield NoContent
+//      val temp = java.io.File.createTempFile("variations", ".html")
+//
+//      println(s"Writing to $temp")
+//
+//      new PrintWriter(temp) { write(page); close }
+//
+//      s"epiphany $temp".!
+//
+//      concurrent.Future.successful(NoContent)
+      for {
+        _ <- gforms.submitToDms(page, sdilNumber)
+        _ <- returnSubmission.save(data, sdilNumber)
+      } yield NoContent
 
     }
   }
