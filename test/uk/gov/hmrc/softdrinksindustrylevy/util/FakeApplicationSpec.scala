2f25b301baf5c8ff23fede5c77e38c1995ab99af
Matt Roberts <mattjr72@gmail.com>
2018-11-20 13:10:35 +0000

    removed commented code

--- test/uk/gov/hmrc/softdrinksindustrylevy/util/FakeApplicationSpec.scala
+++ test/uk/gov/hmrc/softdrinksindustrylevy/util/FakeApplicationSpec.scala
@@ -73,7 +73,6 @@ trait FakeApplicationSpec extends PlaySpec with BaseOneAppPerSuite with FakeAppl
         }
     }
 
-//    override def subscriptions: SubsDAO[String, Subscription] = new SubsDAO[String, Subscription] {
     val subscriptions: SubsDAO[String, Subscription] = new SubsDAO[String, Subscription] {
       private var data: scala.collection.mutable.Map[String, List[Subscription]] = mutable.Map.empty
 
