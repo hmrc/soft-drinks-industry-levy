407d95cf0b47d987bcc0b2834cb2ac71ed8849ef
Priyanka Dhiman <88194246+priyankadhiman3006@users.noreply.github.com>
2022-10-21 11:57:45 +0100

    DLS-6142 unit test cases

--- test/uk/gov/hmrc/softdrinksindustrylevy/services/ReturnsVariationSubmissionServiceSpec.scala
+++ test/uk/gov/hmrc/softdrinksindustrylevy/services/ReturnsVariationSubmissionServiceSpec.scala
@@ -17,71 +17,83 @@
 package uk.gov.hmrc.softdrinksindustrylevy.services
 
 import org.mockito.Mockito.when
-import org.scalatest.concurrent.ScalaFutures
-
-import java.time.Instant
 import org.scalatest.BeforeAndAfterEach
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.mockito.MockitoSugar
 import play.modules.reactivemongo.ReactiveMongoComponent
 import reactivemongo.bson.BSONObjectID
-import uk.gov.hmrc.softdrinksindustrylevy.models.{UkAddress, VariationsRequest}
+import uk.gov.hmrc.softdrinksindustrylevy.models.{ReturnsVariationRequest, UkAddress}
 import uk.gov.hmrc.softdrinksindustrylevy.util.{FakeApplicationSpec, MongoConnectorCustom}
-import scala.language.postfixOps
 
+import java.time.Instant
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
-class VariationSubmissionServiceSpec
+class ReturnsVariationSubmissionServiceSpec
     extends FakeApplicationSpec with MockitoSugar with BeforeAndAfterEach with ScalaFutures with MongoConnectorCustom {
 
-  implicit val defaultTimeout: FiniteDuration = 5 seconds
+  implicit val defaultTimeout: FiniteDuration = 5.seconds
 
   def await[A](future: Future[A])(implicit timeout: Duration): A = Await.result(future, timeout)
 
   implicit val mc: ReactiveMongoComponent = mock[ReactiveMongoComponent]
   when(mc.mongoConnector).thenReturn(mongoConnector)
-  private val service = new VariationSubmissionService
+  private val service: ReturnsVariationSubmissionService = new ReturnsVariationSubmissionService()
+  val address = UkAddress(List("My House", "My Lane"), "AA111A")
+  val tradingName = "Generic Soft Drinks Company Inc Ltd LLC Intl GB UK"
+  val returnVariationRequest =
+    ReturnsVariationRequest(
+      orgName = tradingName,
+      ppobAddress = address,
+      importer = (false, (0, 0)),
+      packer = (false, (0, 0)),
+      warehouses = Nil,
+      packingSites = Nil,
+      phoneNumber = "",
+      email = "email",
+      taxEstimation = BigDecimal("1.1")
+    )
+  val sdilRef = "XCSDIL000000000"
+  val sdilRef1 = "XCSDIL000000001"
 
   override def beforeEach() {
     service.drop
   }
 
-  val address = UkAddress(List("My House", "My Lane"), "AA111A")
-  val tradingName = "Generic Soft Drinks Company Inc Ltd LLC Intl GB UK"
-  val variationsRequest =
-    VariationsRequest(tradingName = Some(tradingName), displayOrgName = tradingName, ppobAddress = address)
-  val sdilRef = "XCSDIL000000000"
-
-  "save" should {
-    "successfully save the data within a VariationWrapper" in {
-      await(service.save(variationsRequest, sdilRef))
-
+  "save method" should {
+    " successfully save the data within ReturnsVariationWrapper" in {
+      await(service.save(returnVariationRequest, sdilRef))
       val storedItem = await(service.find()).head
-
-      storedItem.isInstanceOf[VariationWrapper] mustBe true
-      storedItem.submission mustBe variationsRequest
+      storedItem.isInstanceOf[ReturnsVariationWrapper] mustBe true
+      storedItem.submission mustBe returnVariationRequest
       storedItem.sdilRef mustBe sdilRef
       storedItem._id.isInstanceOf[BSONObjectID] mustBe true
       storedItem.timestamp.isInstanceOf[Instant] mustBe true
     }
   }
 
-  "get" should {
-    "retrieve a match of existing variaionRequest when looked up using sdilRef" in {
-      await(service.save(variationsRequest, sdilRef))
-
-      val storedItem = await(service.get(sdilRef)).get
-      storedItem mustBe variationsRequest
+  "get method" should {
+    "retrieve None when there are no records in the db" in {
+      val storedItem = await(service.get(sdilRef))
+      storedItem mustBe None
     }
 
-    "assure the result is headOption from list when sortedWith _.timestamp isAfter _.timestamp" in {
+    "retrieve One record that matches the sdilRef " in {
       val tradingNameToCompare: String = "checkOrderOfThisObjectCreationUsingThis"
-      await(service.save(variationsRequest, sdilRef))
-      await(service.save(variationsRequest.copy(tradingName = Some(tradingNameToCompare)), sdilRef))
-
+      await(service.save(returnVariationRequest, sdilRef))
+      await(service.save(returnVariationRequest.copy(orgName = tradingNameToCompare), sdilRef1))
       val storedItem = await(service.get(sdilRef)).get
-      storedItem.tradingName.get mustBe tradingNameToCompare
+      storedItem mustBe returnVariationRequest
+    }
+
+    "retrieve the first record if more than one records are found (sorted by timestamp descending)" in {
+      val tradingNameToCompare: String = "checkOrderOfThisObjectCreationUsingThis"
+      await(service.save(returnVariationRequest, sdilRef))
+      await(service.save(returnVariationRequest.copy(orgName = tradingNameToCompare), sdilRef))
+      val storedItem = await(service.get(sdilRef)).get
+      storedItem.orgName mustBe tradingNameToCompare
     }
   }
+
 }
