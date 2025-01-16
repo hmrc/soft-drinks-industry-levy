3c0912f4af82b10d032279cca2b1e8235ff82bcf
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2025-01-16 16:24:26 +0000

    DLS-11191: LitresOps calls Rates.getLevyCalculation

--- app/uk/gov/hmrc/softdrinksindustrylevy/models/package.scala
+++ app/uk/gov/hmrc/softdrinksindustrylevy/models/package.scala
@@ -22,7 +22,7 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import play.api.libs.functional.syntax.unlift
 import sdil.models._
-import uk.gov.hmrc.softdrinksindustrylevy.config.Rates
+import uk.gov.hmrc.softdrinksindustrylevy.config.{LevyCalculation, Rates}
 
 package object models {
 
@@ -37,9 +37,10 @@ package object models {
   }
 
   implicit class LitreOps(litreBands: LitreBands)(implicit returnPeriod: ReturnPeriod) {
-    lazy val lowLevy: BigDecimal = litreBands._1 * Rates.lowerBandCostPerLitre
-    lazy val highLevy: BigDecimal = litreBands._2 * Rates.higherBandCostPerLitre
-    lazy val dueLevy: BigDecimal = lowLevy + highLevy
+    lazy val levyCalculation: LevyCalculation = Rates.getLevyCalculation(litreBands._1, litreBands._2, returnPeriod)
+    lazy val lowLevy: BigDecimal = levyCalculation.lowLevy
+    lazy val highLevy: BigDecimal = levyCalculation.highLevy
+    lazy val dueLevy: BigDecimal = levyCalculation.total
   }
 
   // cos coupling is bad, mkay
