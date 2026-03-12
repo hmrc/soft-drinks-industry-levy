a455a4edee9517687718559befaff3038e97f7c9
ShivaniJain-21 <180167518+ShivaniJain-21@users.noreply.github.com>
2026-03-12 12:20:32 +0000

    DLS-12758 [SDIL] Upgrade to bootstrap-play (10.6.0)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   private val playVersion = "play-30"
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
   private val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
@@ -12,7 +12,7 @@ object AppDependencies {
     "uk.gov.hmrc"              %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
     "uk.gov.hmrc.mongo"        %% s"hmrc-mongo-$playVersion"        % hmrcMongoVersion,
     "com.github.fge"           %  "json-schema-validator"           % "2.2.6",
-    "org.typelevel"            %% "cats-core"                       % "2.12.0",
+    "org.typelevel"            %% "cats-core"                       % "2.13.0",
     "org.scala-stm"            %% "scala-stm"                       % "0.11.1",
     "io.github.openhtmltopdf"  % "openhtmltopdf-pdfbox"             % "1.1.31"
   )
@@ -21,10 +21,10 @@ object AppDependencies {
     "uk.gov.hmrc"                %% s"bootstrap-test-$playVersion"  % bootstrapVersion,
     "uk.gov.hmrc"                %% "stub-data-generator"           % "1.6.0",
     "uk.gov.hmrc.mongo"          %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion,
-    "org.wiremock"               % "wiremock"                       % "3.13.1",
-    "com.fasterxml.jackson.core" %  "jackson-core"                  % "2.20.0",
-    "com.fasterxml.jackson.core" %  "jackson-databind"              % "2.20.0",
-    "org.jsoup"                  %  "jsoup"                         % "1.21.2",
+    "org.wiremock"               % "wiremock"                       % "3.13.2",
+    "com.fasterxml.jackson.core" %  "jackson-core"                  % "2.21.1",
+    "com.fasterxml.jackson.core" %  "jackson-databind"              % "2.21.1",
+    "org.jsoup"                  %  "jsoup"                         % "1.22.1",
     "org.scalatest"              %% "scalatest-funsuite"            % "3.2.19",
     "org.scalatestplus"          %% "mockito-5-18"                  % "3.2.19.0",
     "org.scalatestplus"          %% "scalacheck-1-17"               % "3.2.18.0",
